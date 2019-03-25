b464cd1c871caf257d471d23a2430ef27c01ed2a
David Stansby <dstansby@gmail.com>
2019-03-25 14:03:47 +0000

    Try an http location

--- tests/test_cdfread.py
+++ tests/test_cdfread.py
@@ -3,10 +3,10 @@ import cdflib
 
 
 def test_read():
-    fname = 'messenger.cdf'
+    fname = 'helios.cdf'
     if not os.path.exists(fname):
         import urllib.request
         urllib.request.urlretrieve(
-            'ftp://spdf.gsfc.nasa.gov/pub/data/messenger/rtn/2010/messenger_mag_rtn_20100105_v01.cdf',
+            'http://helios-data.ssl.berkeley.edu/data/E1_experiment/New_proton_corefit_data_2017/cdf/helios1/1974/h1_1974_346_corefit.cdf',
             fname)
     cdf = cdflib.CDF(fname)
