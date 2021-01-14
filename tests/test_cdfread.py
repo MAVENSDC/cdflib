4bdc5288d0a8e137ded7fed5d1aed6e4848b7d4e
= <=>
2021-01-13 20:09:30 -0500

    updated cdfread to use self in functions that call it

--- tests/test_cdfread.py
+++ tests/test_cdfread.py
@@ -5,9 +5,9 @@ import cdflib
 
 def test_read():
     fname = 'helios.cdf'
+    url = '''http://helios-data.ssl.berkeley.edu/data/
+             E1_experiment/New_proton_corefit_data_2017/
+             cdf/helios1/1974/h1_1974_346_corefit.cdf'''
     if not os.path.exists(fname):
-        urllib.request.urlretrieve(
-            'http://helios-data.ssl.berkeley.edu/data/E1_experiment/New_proton_corefit_data_2017/cdf/helios1/1974/h1_1974_346_corefit.cdf',
-            fname,
-        )
-    cdf = cdflib.CDF(fname)
+        urllib.request.urlretrieve(url, fname)
+    cdflib.CDF(fname)
