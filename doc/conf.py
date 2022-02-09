b9ad33daa6eb73f83c3b6fe29af60e2cdbc874a2
Bryan Harter <harter864@gmail.com>
2022-02-09 16:26:31 -0700

    Trying to determine why documentation import fails

--- doc/conf.py
+++ doc/conf.py
@@ -166,3 +166,4 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/3': None}
+autodoc_mock_imports = ['xarray', 'astropy']
