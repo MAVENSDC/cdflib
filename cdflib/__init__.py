dac49897b02a35949dffe2a23d92aff722ab0879
Bryan Harter <harter864@gmail.com>
2022-02-17 16:03:06 -0700

    Fixing the unit tests (and maybe the docs?)

--- cdflib/__init__.py
+++ cdflib/__init__.py
@@ -1,14 +1,6 @@
 from . import cdfread, cdfwrite
 from .epochs import CDFepoch as cdfepoch  # noqa: F401
 
-try:
-    from .epochs_astropy import CDFAstropy as cdfastropy
-
-except Exception:
-    pass
-
-from .cdf_to_xarray import cdf_to_xarray
-from .xarray_to_cdf import xarray_to_cdf
 from pathlib import Path
 
 # This function determines if we are reading or writing a file
@@ -25,3 +17,10 @@ def CDF(path, cdf_spec=None, delete=False, validate=None, string_encoding='ascii
             return cdfread.CDF(path, validate=validate, string_encoding=string_encoding)
     else:
         return cdfwrite.CDF(path, cdf_spec=cdf_spec, delete=delete)
+
+try:
+    from .epochs_astropy import CDFAstropy as cdfastropy
+    from .cdf_to_xarray import cdf_to_xarray
+    from .xarray_to_cdf import xarray_to_cdf
+except Exception:
+    pass
