2169b9e2c0b78badcc331fdc40411eb04f13f384
David Stansby <dstansby@gmail.com>
2022-02-18 17:05:31 +0000

    Keep CDF file open

--- tests/test_cdfread.py
+++ tests/test_cdfread.py
@@ -2,14 +2,20 @@ import pathlib
 
 import cdflib
 
+fname = (pathlib.Path(__file__) / '..' / 'testfiles' /
+         'psp_fld_l2_mag_rtn_1min_20200104_v02.cdf')
+
 
 def test_read():
-    fname = (pathlib.Path(__file__) / '..' / 'testfiles' /
-             'psp_fld_l2_mag_rtn_1min_20200104_v02.cdf')
     cdf = cdflib.CDF(fname)
-    info = cdf.cdf_info()
 
+    info = cdf.cdf_info()
     assert isinstance(info, dict)
 
     varatts = cdf.varattsget('psp_fld_l2_mag_RTN_1min')
     assert isinstance(varatts, dict)
+
+
+def test_context():
+    with cdflib.CDF(fname) as cdf:
+        cdf.cdf_info()
