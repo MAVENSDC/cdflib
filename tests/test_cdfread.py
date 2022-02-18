9489a91427beccbe56e542c094a823867212f05d
David Stansby <dstansby@gmail.com>
2022-02-18 15:41:47 +0000

    Test varattsget

--- tests/test_cdfread.py
+++ tests/test_cdfread.py
@@ -6,4 +6,10 @@ import cdflib
 def test_read():
     fname = (pathlib.Path(__file__) / '..' / 'testfiles' /
              'psp_fld_l2_mag_rtn_1min_20200104_v02.cdf')
-    cdflib.CDF(fname)
+    cdf = cdflib.CDF(fname)
+    info = cdf.cdf_info()
+
+    assert isinstance(info, dict)
+
+    varatts = cdf.varattsget('psp_fld_l2_mag_RTN_1min')
+    assert isinstance(varatts, dict)
