3149d344e0f7c7cdf6f64ff0fb36d02b82887ba1
Bryan Harter <harter864@gmail.com>
2022-03-08 13:18:54 -0700

    Updating the version number

--- cdflib/__init__.py
+++ cdflib/__init__.py
@@ -6,8 +6,13 @@ from .cdf_factory import CDF
 from .epochs import CDFepoch as cdfepoch  # noqa: F401
 
 try:
-    # These are optional dependencies 
+    # This is an optional dependency for astropy time conversions
     from .epochs_astropy import CDFAstropy as cdfastropy
+except:
+    pass
+
+try:
+    # Another optional dependency for XArray <-> cdf conversions
     from .xarray_to_cdf import xarray_to_cdf
     from .cdf_to_xarray import cdf_to_xarray
 except:
