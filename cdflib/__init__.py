3c98233a6a5a3593e648660cdf878af889fd8e12
Bryan Harter <harter864@gmail.com>
2022-03-08 13:14:14 -0700

    Making xarray/astropy importing optional when importing cdflib

--- cdflib/__init__.py
+++ cdflib/__init__.py
@@ -2,9 +2,15 @@ from pathlib import Path
 
 from . import cdfread, cdfwrite
 from .cdf_factory import CDF
-from .cdf_to_xarray import cdf_to_xarray
+
 from .epochs import CDFepoch as cdfepoch  # noqa: F401
-from .epochs_astropy import CDFAstropy as cdfastropy
-from .xarray_to_cdf import xarray_to_cdf
+
+try:
+    # These are optional dependencies 
+    from .epochs_astropy import CDFAstropy as cdfastropy
+    from .xarray_to_cdf import xarray_to_cdf
+    from .cdf_to_xarray import cdf_to_xarray
+except:
+    pass
 
 __all__ = ['CDF', 'xarray_to_cdf', 'cdf_to_xarray']
