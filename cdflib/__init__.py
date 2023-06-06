42113428277c22744f5d679b653ceab0e939e381
David Stansby <dstansby@gmail.com>
2023-06-06 16:42:16 +0100

    Convert cdf_to_xarray prints to logger

--- cdflib/__init__.py
+++ cdflib/__init__.py
@@ -1,10 +1,15 @@
+import logging
 from pathlib import Path
 
 from cdflib.cdfread import CDF
 from . import cdfread, cdfwrite
 from .epochs import CDFepoch as cdfepoch  # noqa: F401
 
+logger = logging.getLogger(__name__)
+
 __all__ = ["CDF"]
+
+
 try:
     from ._version import version as __version__
 except Exception:
